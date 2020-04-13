0a85148a298dac3690712a027a85f35e419a72e2
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-04-13 10:20:38 +0800

    set up action creators, initialized state of reducers with initialState, refactored code to reflect redux architecture

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Header = props => {
   return (
-    <>
+    <div>
       <figure className="image is-128x128">
         <img src={props.car.image} alt={props.car.name} />
       </figure>
       <h2>{props.car.name}</h2>
       <p>Amount: ${props.car.price}</p>
-    </>
+    </div>
   );
 };
 
